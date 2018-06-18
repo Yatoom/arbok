ff767f007b8604cece4ba70103341fff22bd0ca4
Yatoom <jmapvhoof@gmail.com>
2018-06-19 00:44:50 +0200

    Keeping track of the feature names.

--- arbok/param_preprocessor.py
+++ arbok/param_preprocessor.py
@@ -7,13 +7,14 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler
 
 
 class ParamPreprocessor(BaseEstimator, TransformerMixin):
-    def __init__(self, types="detect", scaler=StandardScaler()):
+    def __init__(self, types="detect", scaler=StandardScaler(), names=None):
 
         # List of types for each column
         self.types = types
         self.mapping = None
         self.one_hot_encoder = None
         self.scaler = scaler
+        self.names = names
 
     def fit(self, X):
         self.fit_transform(X, )
@@ -25,8 +26,8 @@ class ParamPreprocessor(BaseEstimator, TransformerMixin):
         mapping = self.mapping
 
         # Preprocessing steps
-        X_, types = ParamPreprocessor._remove_unsupported(X_, types)
-        X_, types = ParamPreprocessor._split_mixed(X_, types)
+        X_, types, _ = ParamPreprocessor._remove_unsupported(X_, types, None)
+        X_, types, _ = ParamPreprocessor._split_mixed(X_, types, None)
         X_ = ParamPreprocessor._nominal_to_numerical(X_, types, mapping)
         X_ = ParamPreprocessor._booleans_to_numerical(X_, types)
         X_ = ParamPreprocessor._fix_null(X_)
@@ -38,6 +39,7 @@ class ParamPreprocessor(BaseEstimator, TransformerMixin):
     def fit_transform(self, X, y=None, **kwargs):
         X_ = np.copy(X)
         types = self.types
+        names = self.names
 
         if self.types == "detect":
             unique = ParamPreprocessor._get_unique(X_)
@@ -45,8 +47,8 @@ class ParamPreprocessor(BaseEstimator, TransformerMixin):
             self.types = types
 
         # Preprocessing steps
-        X_, types = ParamPreprocessor._remove_unsupported(X_, types)
-        X_, types = ParamPreprocessor._split_mixed(X_, types)
+        X_, types, names = ParamPreprocessor._remove_unsupported(X_, types, names)
+        X_, types, names = ParamPreprocessor._split_mixed(X_, types, names)
         distinct = ParamPreprocessor._get_unique(X_)
         mapping = ParamPreprocessor._create_mapping(distinct, types)
         X_ = ParamPreprocessor._nominal_to_numerical(X_, types, mapping)
@@ -64,10 +66,36 @@ class ParamPreprocessor(BaseEstimator, TransformerMixin):
                                              sparse=False)
         X_ = self.one_hot_encoder.fit_transform(X_)
 
+        # Expand names according to one hot encoding and store result
+        self.names = self.one_hot_encode_names(categorical_features, self.one_hot_encoder.n_values, names)
+
         # Fit scaler
         X_ = self.scaler.fit_transform(X_)
 
         return X_
+
+    @staticmethod
+    def one_hot_encode_names(categorical_indices, categorical_n_values, names):
+
+        # Check if names is None
+        if names is None:
+            return None
+
+        # Convert mask to array of indices if needed
+        if isinstance(categorical_indices[0], bool):
+            categorical_indices = np.where(categorical_indices)[0].tolist()
+
+        result = []
+        for index, name in enumerate(names):
+            if index in categorical_indices:
+                amount = categorical_n_values[categorical_indices.index(index)]
+
+                for i in range(amount):
+                    result.append(f"{name}__{i}")
+            else:
+                result.append(name)
+
+        return result
 
     @staticmethod
     def _detect_types(unique):
@@ -90,20 +118,27 @@ class ParamPreprocessor(BaseEstimator, TransformerMixin):
 
     # Doesn't need fitting
     @staticmethod
-    def _remove_unsupported(X, types):
+    def _remove_unsupported(X, types, names):
         indices = [index for index, value in enumerate(types) if
                    value not in ["mixed", "numerical", "bool", "nominal"]]
         # indices = np.where(np.array(types) not in ["mixed", "numerical", "bool", "nominal"])[0]
         new_X = np.delete(X, indices, 1)
         new_types = np.delete(types, indices, 0).tolist()
-        return new_X, new_types
+
+        # Update names
+        new_names = None
+        if names is not None:
+            new_names = np.delete(names, indices).tolist()
+
+        return new_X, new_types, new_names
 
     # Doesn't need fitting
     @staticmethod
-    def _split_mixed(X, types):
+    def _split_mixed(X, types, names):
         indices = np.where(np.array(types) == "mixed")[0]
         columns = np.copy(X.T)
         new_types = types
+        new_names = names
         for i in indices:
             # Get numerical and nominal
             col = columns[i]
@@ -118,9 +153,14 @@ class ParamPreprocessor(BaseEstimator, TransformerMixin):
             new_types[i] = "numerical"
             new_types.append("nominal")
 
+            # Update names as well
+            if names is not None:
+                new_names[i] = f"{names[i]}__numerical"
+                new_names.append(f"{names[i]}__nominal")
+
         new_X = columns.T
 
-        return new_X, new_types
+        return new_X, new_types, new_names
 
     # Needs a map created during fitting
     @staticmethod
